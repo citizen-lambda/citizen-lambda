a77d4ebbe7b399b64340f22eb1d0d0df204eaa2d
patkap <patkap@users.noreply.github.com>
2019-10-19 12:05:32 +0200

    Linted and commented webpack conf

--- frontend/webpack.server.config.js
+++ frontend/webpack.server.config.js
@@ -1,27 +1,27 @@
 // Work around for https://github.com/angular/angular-cli/issues/7200
 
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "none",
+  mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
-    server: "./server.ts"
+    server: './server.ts'
   },
-  target: "node",
-  resolve: { extensions: [".ts", ".js"] },
+  target: 'node',
+  resolve: { extensions: ['.ts', '.js'] },
   optimization: {
     minimize: false
   },
   output: {
     // Puts the output at the root of the dist folder
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
   },
   module: {
     rules: [
-      { test: /\.ts$/, loader: "ts-loader" },
+      { test: /\.ts$/, loader: 'ts-loader' },
       {
         // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
         // Removing this will cause deprecation warnings to appear.
@@ -34,13 +34,13 @@ module.exports = {
     new webpack.ContextReplacementPlugin(
       // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?angular(\\|\/)core(.+)?/,
-      path.join(__dirname, "src"), // location of your src
+      path.join(__dirname, 'src'), // location of your src
       {} // a map of your routes
     ),
     new webpack.ContextReplacementPlugin(
       // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?express(\\|\/)(.+)?/,
-      path.join(__dirname, "src"),
+      path.join(__dirname, 'src'),
       {}
     )
     // new webpack.ProvidePlugin({
@@ -51,6 +51,12 @@ module.exports = {
     //     )
     //   )
     //   // ...
+    // }),
+    // Bootstrap 4 has a dependency over jQuery & Popper.js:
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery',
+    //   Popper: ['popper.js', 'default']
     // })
   ]
 };
