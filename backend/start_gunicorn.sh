8d53609a5c4d1426de4411a0d99183a018e028e4
patkap <patkap@users.noreply.github.com>
2019-11-05 07:01:47 +0100

    Updates gunicorn launcher

--- backend/start_gunicorn.sh
+++ backend/start_gunicorn.sh
@@ -2,26 +2,27 @@
 
 FLASKDIR=$(readlink -e "${0%/*}")
 APP_DIR="$(dirname "$FLASKDIR")"
-venv_dir="venv"
+venv_dir="/home/pat/.local/share/venv/citizen_prod"
+LOG_DIR="$APP_DIR/var/log"
 
 echo "Starting $app_name"
 echo "FLASKDIR: $FLASKDIR"
 echo "APP_DIR: $APP_DIR"
+echo "VENV: $venv_dir"
+echo "LOG_DIR: $LOG_DIR"
 
 # activate the virtualenv
-source $FLASKDIR/$venv_dir/bin/activate
-echo "VENV: $FLASKDIR/$venv_dir/bin/activate"
+source $venv_dir/bin/activate
 
 cd $FLASKDIR
 
 # Start your gunicorn
-LOG_DIR="$APP_DIR/var/log"
-echo "LOAG_DIR: $LOG_DIR"
 if [[ ! -e $LOG_DIR ]]; then
     mkdir -p $LOG_DIR
 elif [[ ! -d $LOG_DIR ]]; then
     echo "LOG_DIR already exists but is not a directory" 1>&2
+    exit 127
 fi
 
 echo "Starting gunicorn"
-exec gunicorn --error-log $APP_DIR/var/log/gn_errors.log --pid="geonature-citizen.pid" -b :5002 --reload -n "geonature-citizen" wsgi:app
+exec gunicorn --error-log $APP_DIR/var/log/gn_errors.log --pid="citizen.pid" -b :5002 --reload -n "citizen" wsgi:app
