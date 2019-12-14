a2f1cfb6f0cfeeee48cea20efefa0486b1073cb3
patkap <patkap@users.noreply.github.com>
2019-12-14 11:17:53 +0100

    Misc css fixes

--- frontend/src/app/components/about/custom/custom.component.ts
+++ frontend/src/app/components/about/custom/custom.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-about-custom',
   templateUrl: '../../../../../../config/custom/frontend/about/custom.component.html',
-  styleUrls: ['../../../../../../config/custom/frontend/about/custom.component.css']
+  styleUrls: ['../../../../../../config/custom/frontend/about/custom.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutCustomComponent implements OnInit {
   constructor() {}
