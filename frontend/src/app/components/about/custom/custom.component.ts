f910587a29cee429dba19508f037771e925a4797
citizenu03bb <citizenu03bb@users.noreply.github.com>
2020-01-08 17:45:11 +0100

    Updates custom about component title

--- frontend/src/app/components/about/custom/custom.component.ts
+++ frontend/src/app/components/about/custom/custom.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ViewEncapsulation, Inject, LOCALE_ID } from '@angular/core';
+import { Component, ViewEncapsulation, Inject, LOCALE_ID } from '@angular/core';
+import { IAppConfig } from '../../../core/models';
+import { AppConfig } from '../../../../conf/app.config';
+import { SeoService } from '../../../services/seo.service';
+
+type AppConfigAbout = Pick<IAppConfig, 'appName'>;
 
 @Component({
   selector: 'app-about-custom',
@@ -6,8 +11,12 @@ import { Component, OnInit, ViewEncapsulation, Inject, LOCALE_ID } from '@angula
   styleUrls: ['../../../../../../config/custom/frontend/about/custom.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AboutCustomComponent implements OnInit {
-  constructor(@Inject(LOCALE_ID) public localeId: string) {}
+export class AboutCustomComponent {
+  readonly appConfig: AppConfigAbout = AppConfig;
 
-  ngOnInit() {}
+  constructor(@Inject(LOCALE_ID) public localeId: string, protected seo: SeoService) {
+    this.seo.setTitle(
+      `${this.localeId.startsWith('fr') ? 'A Propos' : 'About'} - ${this.appConfig.appName}`
+    );
+  }
 }
