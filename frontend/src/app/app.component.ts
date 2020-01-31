bd253e8f9135a31b7d89d325e1afc39cbed3dca8
citizenu03bb <citizenu03bb@users.noreply.github.com>
2020-01-31 15:17:18 +0100

    Notifies ui of webapp updates

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation, Inject, LOCALE_ID } from '@angular/core';
+import { Component, ViewEncapsulation, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 import { IAppConfig } from './core/models';
 import { AppConfig } from '../conf/app.config';
@@ -12,16 +13,35 @@ type AppConfigApp = Pick<IAppConfig, 'FRONTEND' | 'appName' | 'SEO'>;
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public AppConfig: AppConfigApp = AppConfig;
 
-  constructor(@Inject(LOCALE_ID) readonly localeId: string, protected seo: SeoService) {
+  constructor(
+    @Inject(LOCALE_ID) readonly localeId: string,
+    protected seo: SeoService,
+    private swUpdate: SwUpdate
+  ) {
     this.seo.setMetaTag({ name: 'application-name', content: this.AppConfig.appName });
-    this.seo.setMetaTag({ name: 'keywords', content: (this.AppConfig.SEO.keywords as { [lang: string]: string })[this.localeId] });
-    this.seo.setMetaTag({ name: 'author', content: (this.AppConfig.SEO.author)});
+    this.seo.setMetaTag({
+      name: 'keywords',
+      content: (this.AppConfig.SEO.keywords as { [lang: string]: string })[this.localeId]
+    });
+    this.seo.setMetaTag({ name: 'author', content: this.AppConfig.SEO.author });
     if (this.AppConfig.SEO['google-site-verification']) {
-      this.seo.setMetaTag({ name: 'google-site-verification', content: (this.AppConfig.SEO['google-site-verification'])});
+      this.seo.setMetaTag({
+        name: 'google-site-verification',
+        content: this.AppConfig.SEO['google-site-verification']
+      });
     }
+  }
 
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available. Load new version ?')) {
+          window.location.reload();
+        }
+      });
+    }
   }
 }
