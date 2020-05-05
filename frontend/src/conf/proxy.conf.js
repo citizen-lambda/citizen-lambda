e3bd6b8540f83cadf54d58ec261e8307d898e34c
citizenu03bb <citizenu03bb@users.noreply.github.com>
2020-05-05 12:16:11 +0200

    Frontend changes
    * resolves EventStream dev proxying in src/conf/proxy.conf.js
    * fix serve:en
    * rm redondant run-script targets from package.json
    * recover firefox thin scrollbar

---
+++ frontend/src/conf/proxy.conf.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-undef */
+const PROXY_CONFIG = {
+  '/api': {
+    target: 'http://localhost:5002',
+    secure: false
+  },
+  '/api/programs/stream': {
+    target: 'http://localhost:5002',
+    secure: false,
+    bypass: function (req, res, proxyOptions) {
+      return 'http://localhost:5002/api/programs/stream';
+    }
+  },
+  logLevel: 'debug'
+};
+
+// eslint-disable-next-line no-undef
+module.exports = PROXY_CONFIG;
